3bf4633ece329b9c77396547d97c46b898e7311e
oderathegreat <oderathegreat@gmail.com>
2022-10-05 11:03:51 +0300

    register activity

--- app/src/main/java/com/example/malishopping/RegisterActivity.kt
+++ app/src/main/java/com/example/malishopping/RegisterActivity.kt
@@ -7,5 +7,8 @@ class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+
+
     }
 }
